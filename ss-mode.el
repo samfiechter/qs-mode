2c00df09bf77384b5959e484f3d47169829f8606
samfiechter <scf@scf-laptop.(none)>
2014-03-26 22:04:06 -0400

    *** empty log message ***

--- ss-mode.el
+++ ss-mode.el
@@ -13,18 +13,18 @@
 
 (defvar ss-mode-empty-name "*Sams Spreadsheet Mode*")
 (defvar ss-mode-column-widths (list ))
-(defvar ss-mode-data [nil])
+(defvar ss-mode-data [ ["do" "Re"] ["me" "fa"]])
 ;; ;;;;;;;;;;;;; keymaps ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar ss-mode-map  (make-sparse-keymap 'ss-mode-map))
 
-(define-key ss-mode-map "n"                'ss-start-game)
-(define-key ss-mode-map "q"                'ss-end-game)
 
-(define-key ss-mode-map [left]        'ss-move-left)
-(define-key ss-mode-map [right]        'ss-move-right)
-(define-key ss-mode-map [up]                'ss-move-up)
-(define-key ss-mode-map [down]        'ss-move-down)
+(define-key ss-mode-map "q"                'ss-close)
+
+;; (define-key ss-mode-map [left]        'ss-move-left)
+;; (define-key ss-mode-map [right]        'ss-move-right)
+;; (define-key ss-mode-map [up]                'ss-move-up)
+;; (define-key ss-mode-map [down]        'ss-move-down)
 
 
 (defvar ss-null-map
@@ -34,6 +34,8 @@
 ;; ;;;;;;;;;;;;; functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; SAM's SES MACROS
 
+(defun ss-close () (interactive)
+(kill-buffer (current-buffer)) )
 
 
 (defun ss-import-csv (filename)
@@ -51,7 +53,7 @@
 	   (if (and (char-equal "\"" (substring cell 0 0))
 		    (char-equal "\"" (substring cell -1 -1)))
 	       (setq cell (substring cell 1 -1)) nil )
-	   (setq row (vconcat row '((formula . cell)) ))
+	   (setq row (vconcat row (list '((formula  cell))) ))
 	  )
 	 (setq ss-mode-data (vconcat ss-mode-data row))
 	 (with-current-buffer (ses-goto-print (x+1) 0))
@@ -72,42 +74,29 @@
   ;;            (ss-active-p)]
   ;;           ))
   (setq tabulated-list-format [("" 4 t)]) ;; 0th row is for numbers
-  (let ((a 0) (w 0))
-    (dotimes (a 48)
+  (let ((a 0) (w nil))
+    (dotimes (a 10)
       (setq w (elt ss-mode-column-widths a))     
-      (setq tabulated-list-format (vconcat tabulated-list-format [ (list (char-to-string (+ ?A a)) (if (w) w 12) t)])) ) )
+      (setq tabulated-list-format (vconcat tabulated-list-format (list (list (char-to-string (+ ?A a)) 
+									 12 t)))) ))
   (setq tabulated-list-padding 0)
   (tabulated-list-init-header) )
 
 (defun ss-draw ()
   (interactive)
-  (ss-listing-command)
+;  (ss-listing-command)GitHub1,azn,wGitHub1,azn,w
   (tabulated-list-print t))
 
 (defun ss-listing-command ()
   (interactive)
   ;;  (ss-mode)
-  (let ((tbl (list) )
-        (i 0) (j 0) (k 1) (ir 0)
-        (tline [1 2 3 4])  (c " "))
-    (dotimes (i 16)
-      (setq ir (aref ss-board i))
-      (if (= 0 ir)
-          (setq c " ")
-	(let ((color (ss-color ir)))
-        (setq c (propertize (number-to-string ir) 'font-lock-face  color )) ))
-      (aset tline j c)
-      (if (= 3 j)
-          (progn
-            (add-to-list 'tbl (list  k (copy-sequence tline) ) 1 (lambda (a b) nil))
-            (setq k (+ 1 k))
-            (setq j 0)
-            )
-        (setq j (+ 1 j)) ))
+  (let ((tbl (list) ))
+    (dotimes (i (length ss-data))
+	(add-to-list 'tbl (list (+ 1 i) (copy sequence) (aref ss-data i)) 1 (lambda (a b) nil)) )
     (setq tabulated-list-entries tbl) ))
 
 ;;;###autoload
-(defun ss-mode ()
+(defun ss-mode-fun ()
   "Open SS mode
      ss-mode keybindings:
      \\<ss-mode-map>
